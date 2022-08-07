b17c43518dddea6f8110badcf060a28f0f6425a8
pablo <you@example.com>
2022-08-06 20:55:52 -0400

    coomuicacion base de datos terminada postgresql

---
+++ sap/webapp/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def bienvenido(request):
+    return HttpResponse('Hola Mundo desde Django')
+
+def despedirse (request):
+    return HttpResponse('Despedida desde Django')
