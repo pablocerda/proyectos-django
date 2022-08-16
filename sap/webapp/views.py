80185998e3263f9e7cb502ce0328f0f33de80e99
pablo <you@example.com>
2022-08-16 17:41:47 -0400

    conexion con tabla base de datos

--- sap/webapp/views.py
+++ sap/webapp/views.py
@@ -2,9 +2,12 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from personas.models import Persona
+
 
 def bienvenido(request):
-    return HttpResponse('Hola Mundo desde Django')
+    no_personas_var = Persona.objects.count()
+    personas = Persona.objects.all()
+    return render(request, 'bienvenido.html', {'no_personas':no_personas_var, 'personas': personas})
 
-def despedirse (request):
-    return HttpResponse('Despedida desde Django')
+
