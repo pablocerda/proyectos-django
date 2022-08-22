7dbb2985a4ea58aae2b7fd35bcbae6c9a78165e3
pablo <you@example.com>
2022-08-22 01:14:43 -0400

    penultima modificacion aplicacion

--- sap/personas/views.py
+++ sap/personas/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, get_object_or_404
+from django.forms import modelform_factory
+from django.shortcuts import render, get_object_or_404, redirect
 
 # Create your views here.
+from personas.forms import PersonaForm
 from personas.models import Persona
 
 
@@ -9,3 +11,15 @@ def detallePersona(request, id):
     persona = get_object_or_404(Persona, pk=id)
     return render(request, 'personas/detalle.html', {'persona': persona})
 
+#PersonaForm = modelform_factory(Persona, exclude=[])
+
+def nuevaPersona(request):
+    if request.method == 'POST':
+        formaPersona = PersonaForm(request.POST)
+        if formaPersona.is_valid():
+            formaPersona.save()
+            return redirect('index')
+    else:
+        formaPersona = PersonaForm()
+
+    return render(request, 'personas/nuevo.html', {'formaPersona': formaPersona})
